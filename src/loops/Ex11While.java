a4a7f7c2300a0a4a26abbe46993a6f57310b259b
VictorPresenca <presenca.victor@gmail.com>
2026-05-12 22:54:45 -0300

    Atualização no exercício 11 de while.

--- src/loops/Ex11While.java
+++ src/loops/Ex11While.java
@@ -4,38 +4,80 @@ import java.util.Scanner;
 public class Ex11While {
     static void main (String[] args) {
 
-        // FUTURA CALCULADORA
-
         // objeto scanner
         Scanner scanner = new Scanner(System.in);
 
         // variáveis
         int escolha;
-        int valor1 = 0;
-        int valor2 = 0;
+        double valor1 = 0;
+        double valor2 = 0;
 
-        System.out.println(
-                "1 - Soma" +
-                "\n2 - Subtração" +
-                "\n3 - Multiplicação" +
-                "\n4 - Divisão" +
-                "\n5 - Sair"
-        );
-        escolha = scanner.nextInt();
+        do {
 
-        valor1 = scanner.nextInt();
-        valor2 = scanner.nextInt();
+            System.out.println("\nVamos calcular! Digite dois valores para fazermos calculos básicos\n");
 
-        // soma
-        System.out.println(valor1 + valor2);
+            System.out.println("Digite o primeiro valor");
+            valor1 = scanner.nextInt();
 
-        // subtração
-        System.out.println(valor1 - valor2);
+            System.out.println("Digite o segundo valor");
+            valor2 = scanner.nextInt();
 
-        // multiplicação
-        System.out.println(valor1 * valor2);
+            System.out.println(
+                    "1 - Soma" +
+                    "\n2 - Subtração" +
+                    "\n3 - Multiplicação" +
+                    "\n4 - Divisão" +
+                    "\n5 - Sair"
+            );
+            escolha = scanner.nextInt();
 
-        // divisão
-        System.out.println(valor1 / valor2);
+            if (escolha == 1) {
+
+                // soma
+                System.out.println(
+                        "A soma dos valores digitados é: " + (valor1 + valor2) +
+                        "\n1 - Fazer outro cálculo" +
+                        "\n5 - Sair"
+                        );
+                escolha = scanner.nextInt();
+
+            } else if (escolha == 2) {
+
+                // subtração
+                System.out.println(
+                        "A subtração dos valores digitados é: " + (valor1 - valor2) +
+                                "\n1 - Fazer outro cálculo" +
+                                "\n5 - Sair"
+                );
+                escolha = scanner.nextInt();
+
+            } else if (escolha == 3) {
+
+                // multiplicação
+                System.out.println(
+                        "A multiplicação dos valores digitados é: " + (valor1 * valor2) +
+                                "\n1 - Fazer outro cálculo" +
+                                "\n5 - Sair"
+                );
+                escolha = scanner.nextInt();
+
+            } else if (escolha == 4) {
+
+                // divisão
+                System.out.println(
+                        "A divisão dos valores digitados é: " + (valor1 / valor2) +
+                                "\n1 - Fazer outro cálculo" +
+                                "\n5 - Sair"
+                );
+                escolha = scanner.nextInt();
+
+            } else if (escolha != 0) {
+
+                System.out.println("Escolha uma opção válida. Essa não existe!");
+
+            }
+
+        } while (escolha != 5);
+
     }
 }
